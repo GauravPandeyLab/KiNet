4ad5db7f74ff7288f699bfcbc27d83749ce0ad51
John Sekar <johnarul.sekar@gmail.com>
2023-07-20 13:59:23 -0400

    temp14

--- load_data.R
+++ load_data.R
@@ -27,6 +27,11 @@ get_pathway_genes <- function(categ,name){
 get_gene_name <- function(protein) {
   all_nodes %>% filter(id==protein) %>% pull(GeneName)
 }
+get_edges <- function(proteins) {
+  E1 <- all_edges %>% filter(from %in% proteins)
+  E2 <- all_edges %>% filter(to %in% proteins)
+  return(rbind(E1,E2))
+}
 get_one_degree <- function(genes) {
   proteins <- all_nodes %>% filter(GeneName %in% genes) %>% pull(id)
   E1 <- all_edges %>% filter(from %in% proteins)
@@ -35,6 +40,26 @@ get_one_degree <- function(genes) {
   g$edges=rbind(E1,E2)
   ids <- c(E1 %>% pull(to), E2 %>% pull(from), proteins) %>% unique
   g$nodes <- all_nodes %>% filter(id %in% ids) %>% mutate(label=GeneName)
+  return(g)
+}
+get_zero_degree <- function(genes) {
+  proteins <- all_nodes %>% filter(GeneName %in% genes) %>% pull(id)
+  E1 <- all_edges %>% filter(from %in% proteins) %>% filter(to %in% proteins)
+  nodes <- all_nodes %>% filter(id %in% proteins) %>% mutate(label=GeneName)
+  g = list(nodes=nodes,edges=E1)
+  return(g)
+}
+
+
+get_pathway_graph <- function(genes,bool_one_degree,bool_remove_disconnected) {
+  proteins <- all_nodes %>% filter(GeneName %in% genes) %>% pull(id)
+  g <- if(bool_one_degree) get_one_degree(genes) else get_zero_degree(genes)
+  if(bool_remove_disconnected) {
+    nonself <- g$edges %>% filter(from != to)
+    ids <- c(nonself %>% pull(from),nonself %>% pull(to))
+    g$nodes <- g$nodes %>% filter(id %in% ids)
+    g$edges <- g$edges %>% filter(from %in% ids) %>% filter(to %in% ids)
+  }
   return(g)
 }
 
@@ -53,7 +78,9 @@ get_synonyms <- function(id) {
 
 
 render_gene_info <- function(geneName) {
+  if (is.null(geneName)) {return('')}
   info <- all_nodes %>% filter(GeneName == geneName)
+  L0 <- list(tags$h4(geneName))
   L1 <-  list(tags$h5('Name'),tags$b(info$FullName))
   L2 <- list(tags$h5('Synonyms'))
   for(x in get_synonyms(info$id)) {
@@ -71,5 +98,5 @@ render_gene_info <- function(geneName) {
     gene_url <- "https://www.ncbi.nlm.nih.gov/gene/%s"
     L3 <- c(L3,list('NCBI Gene: ',tags$a(info$GeneID,href= sprintf(gene_url,info$GeneID)),tags$br()))
   }
-  tagList(L1,L2,L3)
+  tagList(L0,L1,L2,L3)
 }
